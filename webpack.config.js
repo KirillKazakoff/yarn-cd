4be2121a0da67e5fd335fa990b30c873db1728c9
keka3avr <kekazavr228@gmail.com>
2021-08-22 21:29:01 +1100

    initial commit, first task completed

---
+++ webpack.config.js
@@ -0,0 +1,90 @@
+const path = require('path'); // Node.js модуль для разрешения путей файлов
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+
+
+module.exports = {
+    target: 'web',
+    entry: './src/index.js',
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+
+    devServer: {
+        contentBase: './',
+        watchContentBase: true,
+        publicPath: '/dist/',
+    },
+
+    devtool: 'source-map',
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader, 'css-loader',
+                ],
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'ts-loader',
+                },
+            },
+            // { test: /\.(jpe?g|png|gif|svg|ico)$/i, use: { loader: "url-loader?name=app/images/[name].[ext]" } },
+            {
+                test: /\.(png|jpg|gif|ttf|ico)$/i,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            outputPath: 'img/',
+                            name: '[name].[ext]',
+                        },
+                    },
+                ],
+            },
+            // {
+            //     test: /\.(html)$/,
+            //     use: ['html-loader']
+            // },
+        ],
+    },
+
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: './index.html',
+        }),
+
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+        }),
+
+        new CopyPlugin({
+            patterns: [
+                { from: "src/img", to: "img" },
+            ],
+        }),
+    ],
+
+};
